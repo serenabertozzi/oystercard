24e8b478d2c877a080982a61facb0fedaaaaafca
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-09-27 15:57:20 +0100

    implemented maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,12 @@
-
 class OysterCard
+  MAX_BALANCE = 90
 attr_reader :balance
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
+    fail "Your balance reached £#{MAX_BALANCE}, it cannot be topped up" if @balance >= MAX_BALANCE
     @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,4 +13,11 @@ describe OysterCard do
         card.top_up(5)
         expect(card.balance).to(eq(5))
     end
+
+    it "raises an error, when trying to top up, but it reached max balance" do
+        max_balance = OysterCard::MAX_BALANCE
+        card = OysterCard.new(0)
+        card.top_up(max_balance)
+        expect {card.top_up(1)}.to raise_error "Your balance reached £#{max_balance}, it cannot be topped up"
+    end
 end
